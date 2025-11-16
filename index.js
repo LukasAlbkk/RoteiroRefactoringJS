af5d794d8872026b769feaf172de9ff32a22a3d0
Lucas <lucasoom3@hotmail.com>
2025-11-16 20:47:45 -0300

    refatoracao_10

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ var gerarFaturaStr = require("./apresentacao.js");
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const calc = new ServicoCalculoFatura(new Repositorio());
 const faturaStr = gerarFaturaStr(faturas, calc);
-console.log(faturaStr);
+console.log(faturaStr); 
